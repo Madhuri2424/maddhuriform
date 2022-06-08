9406c520d865e2430cc702802fe58185f108fa76
Madhuri2424 <107117007+Madhuri2424@users.noreply.github.com>
2022-06-08 18:43:52 +0530

    Add files via upload

---
+++ FormInput.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./formInput.css";
+import Select from 'react-select'
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      
+      <span>{errorMessage}</span>
+      
+    </div>
+  );
+};
+
+export default FormInput;
